afe0b9c55b2d4f0d517b0edaf28837264823cdb6
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-11 18:59:02 -0400

    Params

---
+++ Parameter Array/ParameterArray/ParameterArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterArray
+{
+
+     
+    class Program
+    {
+        static int sumNums(params int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i <= nums.GetUpperBound(0); i++)
+            {
+                sum += nums[i];
+                return sum;
+            }
+      
+        }
+        static void Main(string[] args)
+        {
+
+
+
+var total = sumNums(1,2,3);
+        }
+    }
+}
