3087a5a4541d309bccfec24c58aa3b638bcd923a
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-10 21:01:28 -0400

    We Start

---
+++ IntStruct/IntStruct/IntStruct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntStruct
+{
+    public class InStruct
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string snum;
+            Console.Write("Enter a number: ");
+            snum = Console.ReadLine();
+            num = Int32.Parse(snum);
+            Console.WriteLine(num);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structure/SimpleStructure/SimpleStructure/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct Name
+{
+    private string fname, mname, lname;
+
+    public Name(string first, string middle, string last)
+    {
+        fname = first;
+        mname = middle;
+        lname = last;
+    }
+    public string firstName
+    {
+        get
+        {
+            return fname;
+        }
+        set
+        {
+            fname = firstName;
+        }
+  }
+
+
+
+    public string middleName
+    {
+        get
+        {
+            return mname;
+        }
+
+        set
+        {
+            mname = middleName; 
+        }
+    }
+
+
+    public string lastName
+    {
+        get
+        {
+            return lname;
+        }
+
+        set
+        {
+            lname = lastName;
+        }
+    }
+
+    public override string ToString()
+    {
+        return (String.Format("{0} {1} {2}", fname, mname, lname));
+    }
+
+    public string Initials()
+    {
+        return (String.Format("{0}{1}{2}", fname, mname, lname));
+    }
+
+}
+
+public class NameTest
+{
+    static void Main()
+    {
+        Name myName = new Name("Michael", "Mason", "McMillan");
+        string fullName, inits;
+        fullName = myName.ToString();
+        inits = myName.ToString();
+        Console.WriteLine("My name is {0}.", fullName);
+        Console.WriteLine("My initials are {0}.", inits);
+        Console.ReadKey();
+    }
+
+}
