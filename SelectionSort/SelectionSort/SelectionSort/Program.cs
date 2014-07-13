86d8d74b15b8f038809d01246ea7c271b448630d
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-13 18:49:11 -0400

    SelectionSort

---
+++ SelectionSort/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int min, temp;
+            for (int outer = 0; outer <= upper; outer++)
+            {
+                min = outer;
+                for (int inner = outer + 1; inner <= upper; inner++)
+                    if (arr[inner] < arr[min])
+                        min = inner;
+                temp = arr[outer];
+                arr[outer] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+}
