65b7eb8e0c23c59f0437cd4ff34ce1bb2cb3b240
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-13 18:40:28 -0400

    BubbleSort

---
+++ BubbleSort/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+                for(int outer = upper; outer >= 1; outer--){
+                    if((int)arr[inner] > arr[inner+1]){
+                        temp = arr[inner];
+                        arr[inner] = arr[inner+1];
+                        arr[inner+1] = temp;
+                    }
+                }
+        }
+    }
+}
